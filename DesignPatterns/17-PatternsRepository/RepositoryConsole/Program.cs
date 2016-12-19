83f8a13ad09f4385d91713dffb09954e031662b3
[417281862@qq.com] <[417281862@qq.com]>
2016-12-19 15:31:17 +0800

    AutoMapper

--- DesignPatterns/17-PatternsRepository/RepositoryConsole/Program.cs
+++ DesignPatterns/17-PatternsRepository/RepositoryConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,43 @@ namespace RepositoryConsole
     {
         static void Main(string[] args)
         {
+            var book = new Book();
+            book.Title = "seeeeeeeee";
+            book.Description = "ddddddddddd";
+            book.Language = "LLLL";
+            book.Authors = new List<Author>();
+            book.Authors.Add(new Author()
+            {
+                Name = "1",
+                Description = "2",
+                ContactInfo = new ContactInfo()
+                {
+                    Blog = "q",
+                    Email = "12@",
+                    Twitter = "t"
+                }
+            });
+            book.Authors.Add(new Author()
+            {
+                Name = "12",
+                Description = "22",
+                ContactInfo = new ContactInfo()
+                {
+                    Blog = "q2",
+                    Email = "12@2",
+                    Twitter = "t2"
+                }
+            });
+
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Book, BookDto>();
+               
+            });
+
+            var exp = Mapper.Map<Book, BookDto>(book);
+
+
             // Construct a ConcurrentDictionary
             ConcurrentDictionary<int, int> cd = new ConcurrentDictionary<int, int>();
 
@@ -32,5 +70,94 @@ namespace RepositoryConsole
             value = cd.GetOrAdd(2, 10000);
             Console.WriteLine("After second GetOrAdd, cd[2] = {0} (should be 100)", value);
         }
+    }
+
+
+    #region E
+
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Language { get; set; }
+
+        public decimal Price { get; set; }
+
+        public List<Author> Authors { get; set; }
+
+        public DateTime? PublishDate { get; set; }
+
+        public Publisher Publisher { get; set; }
+
+        public int? Paperback { get; set; }
+
+    }
+
+    public class Publisher
+    {
+        public string Name { get; set; }
+
+    }
+
+    public class Author
+    {
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ContactInfo ContactInfo { get; set; }
+
+    }
+
+    public class ContactInfo
+    {
+        public string Email { get; set; }
+
+        public string Blog { get; set; }
+
+        public string Twitter { get; set; }
+
+    }
+    #endregion
+
+    public class BookDto
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Language { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime? PublishDate { get; set; }
+
+        public string Publisher { get; set; }
+
+        public int? Paperback { get; set; }
+
+        public string FirstAuthorName { get; set; }
+
+        public string FirstAuthorDescription { get; set; }
+
+        public string FirstAuthorEmail { get; set; }
+
+        public string FirstAuthorBlog { get; set; }
+
+        public string FirstAuthorTwitter { get; set; }
+
+        public string SecondAuthorName { get; set; }
+
+        public string SecondAuthorDescription { get; set; }
+
+        public string SecondAuthorEmail { get; set; }
+
+        public string SecondAuthorBlog { get; set; }
+
+        public string SecondAuthorTwitter { get; set; }
+
     }
 }
---
+++ DesignPatterns/Common.Net.Security/Strategy/DES.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Common.Net.Security
+{
+    /// <summary>
+    /// DES对称加密算法
+    /// </summary>
+    public class DES : EncryptStrategy
+    {
+        /// <summary>
+        /// 解密方法
+        /// </summary>
+        /// <param name="source">解密字符串</param>
+        /// <param name="key">密匙</param>
+        /// <returns></returns>
+        public override string Decrypt(string source, string key)
+        {
+            if (string.IsNullOrEmpty(source) || !Regex.IsMatch(source, @"^[\w\d]+$"))
+                return string.Empty;
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+            //Put  the  input  string  into  the  byte  array  
+            byte[] inputByteArray = new byte[source.Length / 2];
+            for (int x = 0; x < source.Length / 2; x++)
+            {
+                int i = (Convert.ToInt32(source.Substring(x * 2, 2), 16));
+                inputByteArray[x] = (byte)i;
+            }
+
+            //建立加密对象的密钥和偏移量，此值重要，不能修改  
+            key = this.MD5Hash(key).Substring(0, 8);
+            des.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            des.IV = ASCIIEncoding.ASCII.GetBytes(key);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+            //Flush  the  data  through  the  crypto  stream  into  the  memory  stream  
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            //Get  the  decrypted  data  back  from  the  memory  stream  
+            //建立StringBuild对象，CreateDecrypt使用的是流对象，必须把解密后的文本变成流对象  
+            //StringBuilder ret = new StringBuilder();
+            return System.Text.Encoding.Default.GetString(ms.ToArray());
+        }
+
+        /// <summary>
+        /// 加密方法
+        /// </summary>
+        /// <param name="source">加密字符串</param>
+        /// <param name="key">密匙</param>
+        /// <returns></returns>
+        public override string Encrypt(string source, string key)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            //把字符串放到byte数组中  
+            //原来使用的UTF8编码，我改成Unicode编码了，不行  
+            byte[] inputByteArray = Encoding.GetEncoding("gb2312").GetBytes(source);
+            //byte[]  inputByteArray=Encoding.Unicode.GetBytes(pToEncrypt);    
+
+            //建立加密对象的密钥和偏移量  
+            //原文使用ASCIIEncoding.ASCII方法的GetBytes方法  
+            //使得输入密码必须输入英文文本  
+            key = this.MD5Hash(key).Substring(0, 8);
+            des.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            des.IV = ASCIIEncoding.ASCII.GetBytes(key);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            //Write  the  byte  array  into  the  crypto  stream  
+            //(It  will  end  up  in  the  memory  stream)  
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            //Get  the  data  back  from  the  memory  stream,  and  into  a  string  
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in ms.ToArray())
+            {
+                ret.AppendFormat("{0:X2}", b);
+            }
+            ret.ToString();
+            return ret.ToString();
+        }
+    }
+}
