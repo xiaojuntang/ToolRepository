73434b9025255120000e7dbbe2bbe765d23f9eed
[417281862@qq.com] <[417281862@qq.com]>
2017-05-17 16:30:24 +0800

    新增委托方法

---
+++ DesignPatterns/DesignPatterns/计算器.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns
+{
+    public enum Operator
+    {
+        Add, Subtract, Multiply, Divide
+    }
+
+    public class 计算器
+    {
+        static void M()
+        {
+            double a = 1;
+            double b = 2;
+            Console.WriteLine("Result:{0}", Calculate(a, b, Operator.Add));
+        }
+
+        public static double Calculate(double a, double b, Operator o)
+        {
+            switch (o)
+            {
+                case Operator.Add:
+                    return Add(a, b);
+                case Operator.Subtract:
+                    return Subtract(a, b);
+                case Operator.Multiply:
+                    return Multiply(a, b);
+                case Operator.Divide:
+                    return Divide(a, b);
+                default:
+                    return 0;
+            }
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Divide(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/计算器委托.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns
+{
+    public delegate double CalculateDelegate(double a, double b);
+
+    public class 计算器委托
+    {
+        static void M()
+        {
+            double a = 1;
+            double b = 2;
+            Console.WriteLine("Result:{0}", Calculate(a, b, Add));
+        }
+
+        public static double Calculate(double a,double b, CalculateDelegate cd)
+        {
+           return cd.Invoke(a, b);
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Divide(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
